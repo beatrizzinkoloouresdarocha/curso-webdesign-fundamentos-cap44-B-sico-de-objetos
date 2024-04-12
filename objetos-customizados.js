5a0dd0eb768a3ff463eeb5623b76cf4f8f71f797
Beatriz Zinko <zinkobeatriz@gmail.com>
2024-04-12 15:02:32 -0300

    fiz ajustes no meu codigo consegui corrigir este erro .

--- objetos-customizados.js
+++ objetos-customizados.js
@@ -1,16 +1,24 @@
 const task1 = {
     nome: "task1",
     createdAt: new Date(),
-    updatedAt:null,
-    completed: false
-};
+    updatedAt: null,
+    completed: false,
+    ChangeName: function(name){
+        console.log(this)
+    }
+}
 
 const task2 = {
     nome: "task2",
     createdAt: new Date(),
-    updatedAt:null,
-    completed: false
+    updatedAt: null,
+    completed: false,
 }
 
-console.log(task1)
-console.log(task1)
+task1.nome = "task 1 updated"; 
+task1.updatedAt = new Date();
+
+task1. ChangeName()
+
+console.log(task1);
+console.log(task2);
