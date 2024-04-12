9a9b4a86cfa86db9ac7656d72d318f0e12ad2840
Beatriz Zinko <zinkobeatriz@gmail.com>
2024-04-12 10:11:38 -0300

    concluido esta etapa funcionou quando coloquei pra rodar sobre minha string 2 com todas as etapas.

--- objetos.js
+++ objetos.js
@@ -12,6 +12,6 @@ console.log (obj2)
 const str = "minha string"
 console.log("str.length")
 
-const str2=  new String("minha string 2")
-console.log(str2)
-console.log(str2.length)
+const str2=  new String("minha string 2");
+console.log(str2);
+console.log(str2.length);
