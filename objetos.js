615057984dd6a8c51ab9e4c6a59dcf6b4d827923
Beatriz Zinko <zinkobeatriz@gmail.com>
2024-04-12 10:06:39 -0300

    criei minha minha string 2 com todas informações concluido mais uma etapa.

--- objetos.js
+++ objetos.js
@@ -8,3 +8,10 @@ obj2["idade"] = 22
 
 console.log (obj1)
 console.log (obj2)
+
+const str = "minha string"
+console.log("str.length")
+
+const str2=  new String("minha string 2")
+console.log(str2)
+console.log(str2.length)
